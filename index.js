9f4508a6ac18b3d4c36e63d5e576526cc2c1540e
Ugur Kazdal <ugur@wesrom.com>
2020-11-22 12:22:06 +0300

    up

--- index.js
+++ index.js
@@ -3,20 +3,30 @@ const path = require("path");
 const fs = require("fs");
 
 //var PUBLIC_KEY_PATH = fs.readFileSync(path.join(__dirname, 'certs/vodafone.pub'), 'utf8');
-const PUBLIC_KEY_PATH = process.env.PUBLIC_KEY_PATH || fs.readFileSync(path.join(__dirname, 'sample/cert.pub'), 'utf8');
+const PUBLIC_KEY_PATH = process.env.PUBLIC_KEY_PATH
+
+if(typeof PUBLIC_KEY_PATH === "undefined"){
+    console.log("Please provide valid public key path")
+    return;
+}
 
 
-module.exports = function decrypt(req, res, next) {
+module.exports = {
+    decrypt: (req, res, next) => {
 
-    let data = JSON.stringify(req.body);
-    let buff = Buffer.from(data);
+        let buff = req.body
+        try{
+            let decrypted = crypto.publicDecrypt(PUBLIC_KEY_PATH, buff);
+            decryptedObj = JSON.parse(decrypted.toString())
+            if (typeof decryptedObj['data'] === "undefined") {
+                res.status(500).send({'message': 'Can not decrypt data, please provide verified private key, if you sure you have correct key, please get in toch with Omega Dijital'});
+                return
+            }
+            req.decryptedObj = decryptedObj;
+            next();
+        }catch(exception){
+            res.status(500).send({'message' : exception});
+        }
 
-    let decrypted = crypto.publicDecrypt(PUBLIC_KEY_PATH,buff);
-    decryptedObj = JSON.parse(decrypted.toString())
-    if(typeof decryptedObj['data'] === "undefined"){
-        res.send({'message' : 'Can not derypt data, please provide verified private key, if you sure you have correct key, please get in toch with Omega Dijital'},500);
-        return
     }
-    req.decryptedObj = decryptedObj;
-    next();
-};
+}
